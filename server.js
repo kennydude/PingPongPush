fd6d4a7a770d2ca8eaab8c70b9f5f6d884fcab3d
Joe Simpson <headbangerkenny@googlemail.com>
2012-12-22 16:16:29 +0000

    Working (barely)

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require("express"),
+	gcm = require('node-gcm'),
 	orm = require("orm");
 
 var authKey = process.env['key'],
-	secret = process.env['secret'];
+	sender = new gcm.Sender(authKey);
+
+if(process.env.VCAP_SERVICES){
+    var env = JSON.parse(process.env.VCAP_SERVICES);
+    var cred = env['mysql-5.1'][0]['credentials'];
+
+    process.env['db'] = "mysql://" + cred['username'] + ":" + cred['password'] + "@" + cred['host'] + "/pingpong"; 
+}
 
 orm.connect( process.env['db'], function(err, db){
 	if(err){
@@ -29,11 +37,6 @@ orm.connect( process.env['db'], function(err, db){
 		res.end("this is ping pong");
 	});
 
-	app.get("/setup/" + secret, function(req, res){
-		// todo: setup db
-		res.end("ok");
-	});
-
 	function ping(data, offset){
 		user.find({
 			"tag" : data['tags'].split(",")
@@ -41,7 +44,7 @@ orm.connect( process.env['db'], function(err, db){
 			"__merge" : {
 				"from" : {
 					"table" : "user_tag",
-					"field" : "tag"
+					"field" : "user_id"
 				},
 				"to" : {
 					"table" : "user",
@@ -52,9 +55,27 @@ orm.connect( process.env['db'], function(err, db){
 			"offset" : offset
 		}, function(err, results){
 			if(err){ console.log(err); }
-			else{
+			else if(results.length > 0){
 
 				// send push
+				message = new gcm.Message();
+				for(k in data){
+					message.addData(k, data[k]);
+				}
+
+				ids = [];
+				results.forEach(function(device){
+					ids.push(device.gcm);
+				});
+
+				sender.send(message, ids, 3, function(result){
+					// TODO: Handle failure
+					console.log(result);
+
+					if(results.length > 900){
+						ping(data, offset + 1000);
+					}
+				});
 
 			}
 		});
@@ -67,9 +88,54 @@ orm.connect( process.env['db'], function(err, db){
 		ping(req.body, 0);
 	});
 
+	// Once we get the user class, we can save the tags
+	function pong_2(me, req, res){
+		// now remove old tags
+		db.driver.remove("user_tag", {"user_id" : me.id}, function(){
+
+			// register new ones
+			var t = req.body['tags'].split(",");
+			console.log(t);
+			t.forEach(function(ta){
+				console.log(ta);
+				x = new tag({
+					"user_id" : me.id,
+					tag : ta
+				});
+				x.save(function(err){
+					console.log(err);
+				});
+			});
+
+			res.end("ok");
+
+		});
+	}
+
 	app.post("/pong/register", function(req, res){
 		// Register a pong
-		res.end("not implemented");
+		try{
+			user.find({"gcm" : req.body['gcm']}, function(err, results){
+				if(results.length > 0){
+					me = results[0];
+					pong_2(me, req, res);
+				} else{
+					me = new user({
+						gcm : req.body['gcm']
+					});
+					me.save(function(err, me){
+						if(!err){
+							pong_2(me, req, res);
+						} else{
+							res.end("error");
+						}
+					});
+				}
+			});
+		} catch(e){
+			console.log(e);
+			res.end("error");
+		}
 	});
 
 	appPort = process.env['VCAP_APP_PORT'] || 3000;
